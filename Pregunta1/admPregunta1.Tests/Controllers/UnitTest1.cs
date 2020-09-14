82efc4fd86ca1f9972d4bd998feb5c91a560d53c
Ronald André Velasco Céspedes <a2019119153@estudiantes.upsa.edu.bo>
2020-09-13 23:59:58 -0400

    Pregunta 1 completa

---
+++ Pregunta1/admPregunta1.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Web.Http;
+using admPregunta1.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pregunta1.Models;
+
+namespace admPregunta1.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            //Arrange
+            VelascoesController controller = new VelascoesController();
+            //Act
+            var listaPersonas = controller.GetVelascoes();
+            //Assert
+            Assert.IsNotNull(listaPersonas);
+        }
+        [TestMethod]
+        public void TestPost()
+        {
+            //Arrange
+            VelascoesController controller = new VelascoesController();
+            Velasco velasco = new Velasco()
+            {
+                VelascoID = 1,
+                FriendofVelasco = "Thaliana Torrez",
+                place = Places.Santa_Cruz,
+                Email = "thalianatorrez@gmail.com",
+                Birthdate = DateTime.Now
+            };
+            //Act
+            IHttpActionResult actionResult = controller.PostVelasco(velasco);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Velasco>;
+            //Assert
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual("DefaultApi", createdResult.RouteName);
+            Assert.IsNotNull(createdResult.RouteValues["id"]);
+        }
+        [TestMethod]
+        public void TestPut()
+        {
+            //Arrange
+            VelascoesController controller = new VelascoesController();
+            Velasco velasco = new Velasco()
+            {
+                VelascoID = 2,
+                FriendofVelasco = "Enrique Nunes",
+                place = Places.Porongo,
+                Email = "enriquenunes@gmail.com",
+                Birthdate = DateTime.Now
+            };
+            //Act
+            IHttpActionResult actionResultPost = controller.PostVelasco(velasco);
+            var result = controller.PutVelasco(velasco.VelascoID, velasco) as StatusCodeResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(StatusCodeResult));
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+        [TestMethod]
+        public void TestDelete()
+        {
+            //Arrange
+            VelascoesController controller = new VelascoesController();
+            Velasco velasco = new Velasco()
+            {
+                VelascoID = 3,
+                FriendofVelasco = "Jimbo Tapia",
+                place = Places.Cotoca,
+                Email = "jimbotapia@gmail.com",
+                Birthdate = DateTime.Now
+            };
+            //Act
+            IHttpActionResult actionResultPost = controller.PostVelasco(velasco);
+            IHttpActionResult actionResultDelete = controller.DeleteVelasco(velasco.VelascoID);
+            //Assert
+            Assert.IsInstanceOfType(actionResultDelete, typeof(OkNegotiatedContentResult<Velasco>));
+        }
+    }
+    }
+}
---
+++ Pregunta1/admPregunta1/Controllers/VelascoesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pregunta1.Models;
+
+namespace admPregunta1.Controllers
+{
+    public class VelascoesController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Velascoes
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View(db.Velascoes.ToList());
+        }
+
+        // GET: Velascoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Velasco velasco = db.Velascoes.Find(id);
+            if (velasco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(velasco);
+        }
+
+        // GET: Velascoes/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Velascoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "VelascoID,FriendofVelasco,place,Email,Birthdate")] Velasco velasco)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Velascoes.Add(velasco);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(velasco);
+        }
+
+        // GET: Velascoes/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Velasco velasco = db.Velascoes.Find(id);
+            if (velasco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(velasco);
+        }
+
+        // POST: Velascoes/Edit/5
+        [Authorize]
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "VelascoID,FriendofVelasco,place,Email,Birthdate")] Velasco velasco)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(velasco).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(velasco);
+        }
+
+        // GET: Velascoes/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Velasco velasco = db.Velascoes.Find(id);
+            if (velasco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(velasco);
+        }
+
+        // POST: Velascoes/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Velasco velasco = db.Velascoes.Find(id);
+            db.Velascoes.Remove(velasco);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
